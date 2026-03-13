d4a9713447cd5113712f5bb801d162d206938bb9
johnoceans <johnooceans@gmail.com>
2026-03-13 12:19:32 +0100

    Ejercicio 1.2 (sustituyendo if por while )

--- src/AppCaracter.java
+++ src/AppCaracter.java
@@ -2,20 +2,45 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Scanner;
 
 public class AppCaracter {
 
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        // Pedimos archivo de entrada
+        System.out.print("Nombre del archivo de entrada: ");
+        String archivoEntrada = scan.nextLine();
 
         // Declaramos el objeto de tipo File que referencia al fichero de entrada
-        File ficheroEntrada = new File("entrada.txt");
+        File ficheroEntrada = new File(archivoEntrada);
 
-        // Declaramos el objeto de tipo File que referencia al fichero de entrada
-        File ficheroSalida = new File("salidaCaracter.txt");
+        if (!ficheroEntrada.exists()) {
+            System.out.println("El archivo de entrada no existe.");
+            return;
+        }
 
         // Abrimos el archivo para leerlo
         // ALTERNATIVA: FileReader lector = new FileReader("entrada.txt");
         FileReader lector = new FileReader(ficheroEntrada);
+
+        // Pedir desplazamiento
+        System.out.print("Introduce el desplazamiento (-25 a 25 siguiendo la tabla ACSII): ");
+        int desplazamiento = scan.nextInt();
+        scan.nextLine();
+
+        if (desplazamiento < -25 || desplazamiento > 25) {
+            System.out.println("El desplazamiento debe estar entre -25 y 25 siguiendo la tabla ASCII.");
+            return;
+        }
+
+        // Pedimos archivo de salida
+        System.out.print("Nombre del archivo de salida: ");
+        String archivoSalida = scan.nextLine();
+
+        // Declaramos el objeto de tipo File que referencia al fichero de entrada
+        File ficheroSalida = new File(archivoSalida);
 
         // Creamos el archivo de salida
         // SI NO EXISTE, LO CREA
@@ -27,26 +52,22 @@ public class AppCaracter {
 
         // Cuando se alcanza EOF (End Of File), se deja de leer
         while ((caracterInt = lector.read()) != -1) {
-            // Convertimos el carácter en formato int a char para poder trabajar con el
+
             char caracter = (char) caracterInt;
 
-            // Convertimos el caracter a mayuscula
-            char mayuscula = Character.toUpperCase(caracter);
-
-            // Mostramos por pantalla
-            System.out.print(mayuscula);
-
-            /*
-             * Escribimos en el fichero de salida.
-             * Se recomienda (si es posible) no almacenar todo el
-             * contenido del fichero en un String, ya que desconocemos
-             * su tamaño (puede ser de mucho tamaño)
-             */
-            escritor.write(mayuscula);
+            if (caracter >= '!') {
+                char desplazado = (char) (caracter + desplazamiento);
+                System.out.print(desplazado);
+                escritor.write(desplazado);
+            } else {
+                System.out.print(caracter);
+                escritor.write(caracter);
+            }
         }
 
         // Cerramos los archivos
         // MUY IMPORTANTE!!!!!!!
+        scan.close();
         lector.close();
         escritor.close();
 
